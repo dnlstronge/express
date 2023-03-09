b718937e86a137808729207b039fcd9a80cc06a3
Dan Stronge <dnlstronge@gmail.com>
2023-03-09 13:51:31 +0000

    [x] - set up listen on port 5001

--- app.js
+++ app.js
@@ -1,2 +1,19 @@
 const app = require("express")();
 //const app = express()
+
+
+/*
+ useful: 
+    -app.get
+    -app.post
+    -app.put
+    -app.delete
+    -app.all
+    -app.use
+    -app.listen
+
+ */
+
+    app.listen(5001, () => {
+        console.log("server is listening on port 5001");
+    });
