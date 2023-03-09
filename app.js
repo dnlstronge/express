8f4970c416ec4d1540b55376d402d3e428afebcf
Dan Stronge <dnlstronge@gmail.com>
2023-03-09 13:57:56 +0000

    [x] - set up first get requests

--- app.js
+++ app.js
@@ -2,18 +2,26 @@ const app = require("express")();
 //const app = express()
 
 
+    app.get("/", (req, res) => {
+        res.send("Homepage")
+    });
+    app.get("/about", (req, res) => {
+        res.send("About me")
+    })
+
+    app.listen(5001, () => {
+        console.log("server is listening on port 5001");
+    });
+
+
 /*
  useful: 
     -app.get
     -app.post
     -app.put
     -app.delete
-    -app.all
-    -app.use
+    -app.all (works with user requests above)
+    -app.use (resp for middleware)
     -app.listen
 
  */
-
-    app.listen(5001, () => {
-        console.log("server is listening on port 5001");
-    });
