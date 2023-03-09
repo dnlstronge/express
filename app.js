93e9c628c1c9f24fa978c1ff001a18ceb07a68f7
Dan Stronge <dnlstronge@gmail.com>
2023-03-09 11:06:08 +0000

    [x] - add conditionals for req.urls

--- app.js
+++ app.js
@@ -5,12 +5,22 @@ const http = require("http");
 // [2] - deal with the request
 
 const server = http.createServer((req, res) => {
-    console.log(req.url)
+    const url = req.url;
+    if(url === "/") {
     res.writeHead(200, {"content-type":"text/html"})
     res.write(
             "<h1>Homepage</h1><div><p>Here you'll find cotten candy and rides and all sorts of surprises</p></div>")
+    req.end()
+        }
+    if(url === "/about") {
+    res.writeHead(200, {"content-type": "text/html"})
+    res.write("<h1>About</h1>")
     res.end()
-        })
+        }} 
+   
+    )
+
+    
    
 
 server.listen(5001)
