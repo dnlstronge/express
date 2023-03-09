d11c63e791d4a355318dda48e6da5f7226d659ac
Dan Stronge <dnlstronge@gmail.com>
2023-03-09 11:57:44 +0000

    [x] add logic to handle reqs for logic + styles + logo

--- app.js
+++ app.js
@@ -1,40 +1,56 @@
 const http = require("http");
 const { readFileSync } = require("fs");
 
-
 // [1] - provide more info to the browser
 // [2] - deal with the request
 
-// get files: 
+// get files:
 
-const homepage = readFileSync("./index.html") // required upon initialisation
+const homePage = readFileSync("./navbar-app/index.html"); // required upon initialisation
+const homeLogo = readFileSync("./navbar-app/logo.svg");
+const homeStyles = readFileSync("./navbar-app/styles.css");
+const homeLogic = readFileSync("./navbar-app/browser-app.js");
 
 const server = http.createServer((req, res) => {
-    const url = req.url;
+  const url = req.url;
 
-    //hompage
+  //hompage
 
-    if(url === "/") {
-        res.writeHead(200, {"content-type":"text/html"})
-        res.write(homepage)
-        res.end()
-        }
-    // about
-    else if(url === "/about") {
-        res.writeHead(200, {"content-type": "text/html"})
-        res.write("<h1>About</h1>")
-        res.end()
-        } 
-    // 404 - resource not found
-    else {
-        res.writeHead(404, {"content-type": "text/html"})
-        res.write("<h1>Ooops - page not found</h1>")
-        res.end()
-     }
-    }
-    )
+  if (url === "/") {
+    res.writeHead(200, { "content-type": "text/html" });
+    res.write(homePage);
+    res.end();
+  }
+  // about
+  else if (url === "/about") {
+    res.writeHead(200, { "content-type": "text/html" });
+    res.write("<h1>About</h1>");
+    res.end();
+  } 
+  // styles
+  else if (url === "/styles.css") {
+    res.writeHead(200, { "content-type": "text/css" });
+    res.write(homeStyles);
+    res.end();
+  }
+  // logo
+  else if (url === "/logo.svg") {
+    res.writeHead(200, { "content-type": "text/svg" });
+    res.write(homeLogo);
+    res.end();
+  } 
+  // logic
+  else if (url === "/browser-app.js") {
+    res.writeHead(200, { "content-type": "text/js" });
+    res.write(homeLogic);
+    res.end();
+  }
+  // 404 - resource not found
+  else {
+    res.writeHead(404, { "content-type": "text/html" });
+    res.write("<h1>Ooops - page not found</h1>");
+    res.end();
+  }
+});
 
-    
-   
-
-server.listen(5001)
+server.listen(5001);
