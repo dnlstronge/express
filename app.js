beff4279ec7367bb26c8638ac8416fce0737174a
Dan Stronge <dnlstronge@gmail.com>
2023-03-09 08:35:26 +0000

    [x] - have server return simple homepage

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const http = require("http");
 
-const server = http.createServer((req, res) => {
-    console.log("user hit server")
-})
 
-server.listen(5000)
+// [1] - provide more info to the browser
+// [2] - deal with the request
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {"content-type":"text/html"})
+    res.write(
+            "<h1>Homepage</h1><div><p>Here you'll find cotten candy and rides and all sorts of surprises</p></div>")
+    res.end()
+        })
+   
+
+server.listen(5001)
