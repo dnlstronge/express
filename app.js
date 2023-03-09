0a521ee4465abc77b069220a7158f38203eaaccc
Dan Stronge <dnlstronge@gmail.com>
2023-03-09 11:17:09 +0000

    [x] - add 404 to server

--- app.js
+++ app.js
@@ -10,20 +10,22 @@ const server = http.createServer((req, res) => {
     //hompage
 
     if(url === "/") {
-    res.writeHead(200, {"content-type":"text/html"})
-    res.write(
-            "<h1>Homepage</h1><div><p>Here you'll find cotten candy and rides and all sorts of surprises</p></div>")
-    res.end()
+        res.writeHead(200, {"content-type":"text/html"})
+        res.write(
+                "<h1>Homepage</h1><div><p>Here you'll find cotten candy and rides and all sorts of surprises</p></div>")
+        res.end()
         }
     // about
     else if(url === "/about") {
-    res.writeHead(200, {"content-type": "text/html"})
-    res.write("<h1>About</h1>")
-    res.end()
+        res.writeHead(200, {"content-type": "text/html"})
+        res.write("<h1>About</h1>")
+        res.end()
         } 
-
+    // 404 - resource not found
     else {
-
+        res.writeHead(404, {"content-type": "text/html"})
+        res.write("<h1>Ooops - page not found</h1>")
+        res.end()
      }
     }
     )
