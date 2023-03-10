73f5327d49f8a288fc78c1038cc145f1e23f1034
Dan Stronge <dnlstronge@gmail.com>
2023-03-10 08:48:16 +0000

    [x] - set up public folder [x] - move files and use

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
-const app = require("express")();
-//const app = express()
+const express = require("express");
+const app = express()
+const path = require("path")
 
+
+
+    app.use(express.static("./public"))
 
     app.get("/", (req, res) => {
-        res.status(200).send("Homepage")
-    });
-    app.get("/about", (req, res) => {
-        res.status(200).send("About me")
+        res.sendFile(path.resolve(__dirname, "./navbar-app/index.html"))
     })
 
     app.all("*", (req, res) => {
-        res.status(404).send("<h1>Error - resource not found</h1>")
+        res.status(404).send("resource not found")
     })
+
 
     app.listen(5001, () => {
         console.log("server is listening on port 5001");
