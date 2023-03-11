d02bdbd9e465be5c113b9cc744b7ea6f3e5cdc8c
Dan Stronge <dnlstronge@gmail.com>
2023-03-11 08:25:58 +0000

    [x] - send link to direct to api

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require("express");
+const { products } = require("./data")
 
 const app = express();
 
 app.get("/", (req, res) => {
-    res.json()
+    res.send(`<h1>Home</h1><a href="/api/products">Products</a>`)
 })
 
 app.listen(5001, () => {
