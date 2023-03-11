73aa09324afc98b22409d1a827ab6297e12b143b
Dan Stronge <dnlstronge@gmail.com>
2023-03-11 08:47:46 +0000

    [x] - map through data to provide altered list of products

--- app.js
+++ app.js
@@ -6,6 +6,15 @@ const app = express();
 app.get("/", (req, res) => {
     res.send(`<h1>Home</h1><a href="/api/products">Products</a>`)
 })
+app.get("/api/products", (req, res) => {
+    const newProducts = products.map((product) => {
+        const {id, name, image} = product
+        return {
+            id, name, image
+        }
+    })
+    res.send()
+})
 
 app.listen(5001, () => {
     console.log("Listening on port 5001")
