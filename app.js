d1c566b4b92b9c9ce4a466c3f027415152d71d50
Dan Stronge <dnlstronge@gmail.com>
2023-03-11 07:59:42 +0000

    [x] - create new file [x] - add port [x] - add get for root, res JSON

--- app.js
+++ app.js
@@ -1,36 +1,11 @@
 const express = require("express");
-const app = express()
-const path = require("path")
 
+const app = express();
 
-// [1] - adding to static assets
-// [2] - SSR and template engine
+app.get("/", (req, res) => {
+    res.json()
+})
 
-// set up static and middleware
-    app.use(express.static("./public"))
-
-    // app.get("/", (req, res) => {
-    //     res.sendFile(path.resolve(__dirname, "./navbar-app/index.html"))
-    // })
-
-    app.all("*", (req, res) => {
-        res.status(404).send("resource not found")
-    })
-
-
-    app.listen(5001, () => {
-        console.log("server is listening on port 5001");
-    });
-
-
-/*
- useful: 
-    -app.get
-    -app.post
-    -app.put
-    -app.delete
-    -app.all (works with user requests above)
-    -app.use (resp for middleware)
-    -app.listen
-
- */
+app.listen(5001, () => {
+    console.log("Listening on port 5001")
+})
---
+++ previous/app.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const app = express()
+const path = require("path")
+
+
+// [1] - adding to static assets
+// [2] - SSR and template engine
+
+// set up static and middleware
+    app.use(express.static("./public"))
+
+    // app.get("/", (req, res) => {
+    //     res.sendFile(path.resolve(__dirname, "./navbar-app/index.html"))
+    // })
+
+    app.all("*", (req, res) => {
+        res.status(404).send("resource not found")
+    })
+
+
+    app.listen(5001, () => {
+        console.log("server is listening on port 5001");
+    });
+
+
+/*
+ useful: 
+    -app.get
+    -app.post
+    -app.put
+    -app.delete
+    -app.all (works with user requests above)
+    -app.use (resp for middleware)
+    -app.listen
+
+ */
