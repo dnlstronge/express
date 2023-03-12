caaf9f2811586c20fca07a3a08a9052ecdc1886d
Dan Stronge <dnlstronge@gmail.com>
2023-03-12 09:03:47 +0000

    [x] - bug fix

--- app.js
+++ app.js
@@ -3,20 +3,23 @@ const app = express();
 
 // req => middleware => res
 
-const logger = () => {
+const logger = (req, res, next) => {
   const method = req.method;
   const url = req.url;
   const time = new Date().getFullYear();
   console.log(method, url, time);
+  res.send("testing 1..2....3")
 };
 // reference function after path, before req/res
+// express passes req/res objects into middleware function but also needs "next" (above)
 
 app.get("/", logger, (req, res) => {
   logger();
   res.send("Home");
+
 });
 
-app.get("/about", (req, res) => {
+app.get("/about", (req, res, next) => {
   logger();
   res.send("About");
 });
