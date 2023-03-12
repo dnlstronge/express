687799b4233dd6e3397cdcb4dafeca6f408dcd30
Dan Stronge <dnlstronge@gmail.com>
2023-03-12 08:53:51 +0000

    [x] - pass new function into get

--- app.js
+++ app.js
@@ -4,17 +4,20 @@ const app = express();
 // req => middleware => res
 
 const logger = () => {
-    const method = req.method;
-    const url = req.url;
-    const time = new Date().getFullYear();
-    console.log(method, url, time)
-}
+  const method = req.method;
+  const url = req.url;
+  const time = new Date().getFullYear();
+  console.log(method, url, time);
+};
+// reference function after path, before req/res
 
-app.get("/", (req, res) => {
+app.get("/", logger, (req, res) => {
+  logger();
   res.send("Home");
 });
 
 app.get("/about", (req, res) => {
+  logger();
   res.send("About");
 });
 
