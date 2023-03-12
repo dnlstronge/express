d7f778e7fc5e06375d23c7901df43fd856e0f409
Dan Stronge <dnlstronge@gmail.com>
2023-03-12 09:09:13 +0000

    [x] - bug fix - you dont need to invoke the MW function inside the get

--- app.js
+++ app.js
@@ -8,19 +8,18 @@ const logger = (req, res, next) => {
   const url = req.url;
   const time = new Date().getFullYear();
   console.log(method, url, time);
-  res.send("testing 1..2....3") // terminate cycle or use next
+//   res.send("testing 1..2....3") // terminate cycle or use next
+  next()
 };
 // reference function after path, before req/res
 // express passes req/res objects into middleware function but also needs "next" (above)
 
 app.get("/", logger, (req, res) => {
-  logger();
   res.send("Home");
 
 });
 
 app.get("/about", (req, res, next) => {
-  logger();
   res.send("About");
 });
 
