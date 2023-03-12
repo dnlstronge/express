5caaa065068c50892c345355ffe69dd3d3960343
Dan Stronge <dnlstronge@gmail.com>
2023-03-12 11:01:52 +0000

    [x] - show morgan applied to all routes

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const app = express();
-const logger = require("./logger")
-const authorize = require("./authorize")
-
+const logger = require("./logger");
+const authorize = require("./authorize");
+const morgan = require("morgan");
 
 // demo using additional middleware functions
 // to use multiple place in array in .use()
@@ -10,12 +10,13 @@ const authorize = require("./authorize")
 // - [1] .use() or route
 // - [2] options: create own / express (ref docs + supply values) / third party (install it)
 
-app.use(express.static("./public")) // .use expects middleware as an agrument e.g static
+//app.use(express.static("./public")); // .use expects middleware as an agrument e.g static
 //app.use([logger, authorize]) // note: executed in order of array
+
+app.use(morgan("tiny"))
 
 app.get("/", (req, res) => {
   res.send("Home");
-
 });
 
 app.get("/about", (req, res, next) => {
@@ -24,24 +25,20 @@ app.get("/about", (req, res, next) => {
 
 app.get("/api/products", (req, res) => {
   res.send("Products");
-
 });
 
 // if you wanted to add multiple MW to an individial route ===>
 
-app.get("/api/items", [logger, authorize], (req, res, next) => {
-  console.log(req.user)
+app.get("/api/items", (req, res, next) => {
+  console.log(req.user);
   res.send("Items");
 });
-
-
 
 app.listen(5005, () => {
   console.log("listening on port 5005");
 });
 
-
-// notes: 
+// notes:
 
 // req => middleware => res
 // applies to all => invokes for any route but order matters here!
