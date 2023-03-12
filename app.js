ff111ce280048fd2443afe854880ad78a5d0d378
Dan Stronge <dnlstronge@gmail.com>
2023-03-12 08:48:16 +0000

    [x] - add variables

--- app.js
+++ app.js
@@ -4,12 +4,15 @@ const app = express();
 // req => middleware => res
 
 app.get("/", (req, res) => {
-    res.send("Home")
-})
+  const method = req.method;
+  const url = req.url;
+  const time = new Date().getFullYear();
+  res.send("Home");
+});
 
 app.get("/about", (req, res) => {
-    res.send("About")
-})
+  res.send("About");
+});
 
 app.listen(5005, () => {
   console.log("listening on port 5005");
