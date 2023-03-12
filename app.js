cf0a35406fa9369911c44b74cacad75ee229b87b
Dan Stronge <dnlstronge@gmail.com>
2023-03-12 08:45:09 +0000

    [x] - add get requests

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 const express = require("express");
 const app = express();
 
+// req => middleware => res
+
+app.get("/", (req, res) => {
+    res.send("Home")
+})
+
+app.get("/about", (req, res) => {
+    res.send("About")
+})
+
 app.listen(5005, () => {
   console.log("listening on port 5005");
 });
