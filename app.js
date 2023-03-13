a71558b4bdf1ff2468a7e7d0ec992697f742e671
Dan Stronge <dnlstronge@gmail.com>
2023-03-13 09:40:17 +0000

    [x] - add omments

--- app.js
+++ app.js
@@ -9,16 +9,21 @@ app.use(express.static("./methods-public"))
 // parse form data
 app.use(express.urlencoded({extended: false}))
 
+
+// - reads data
 app.get("/api/people", (req, res) => {
   res.status(200).json({success: true, data: people})
 })
+// just because URLs the same doesn't mean the route is as the methods are diff
+// - adds data
+app.post("./api.people", (req, res) => {})
 
 app.post("/login", (req, res) => {
   const { name } = req.body
   if(name) {
     return res.status(200).send(`Welcome ${name}`)
   }
-  res.send("POST")
+  res.status(401).send("Please provide user credentials");
 })
 
 app.listen(5005, () => {
