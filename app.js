c9052ee7ff48d274bc9bba6cd930708f7c28e3ad
Dan Stronge <dnlstronge@gmail.com>
2023-03-13 08:48:42 +0000

    [x] - create get & path

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const express = require("express");
 const app = express();
+let { people } = require("./data");
+
+app.get("./api/people", (req, res) => {
+  res.status(200).json({success: true})
+})
 
 app.listen(5005, () => {
   console.log("listening on port 5005");
