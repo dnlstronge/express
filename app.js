3424850c8b8080ee0976cfa32a9b6ac1a5dc6a4f
Dan Stronge <dnlstronge@gmail.com>
2023-03-13 08:59:36 +0000

    [x] - add static assets folder

--- app.js
+++ app.js
@@ -2,8 +2,13 @@ const express = require("express");
 const app = express();
 let { people } = require("./data");
 
-app.get("./api/people", (req, res) => {
-  res.status(200).json({success: true})
+
+// static assets: 
+
+app.use(express.static("./methods-public"))
+
+app.get("/api/people", (req, res) => {
+  res.status(200).json({success: true, data: people})
 })
 
 app.listen(5005, () => {
