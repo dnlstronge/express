2c2edab55718261b142e467b038609201446bd81
Dan Stronge <dnlstronge@gmail.com>
2023-03-13 10:34:43 +0000

    [x] - change send to json

--- app.js
+++ app.js
@@ -2,35 +2,35 @@ const express = require("express");
 const app = express();
 let { people } = require("./data");
 
+// static assets:
 
-// static assets: 
-
-app.use(express.static("./methods-public"))
+app.use(express.static("./methods-public"));
 // parse form data
-app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({ extended: false }));
 // parse json (more middleware)
-app.use(express.json())
+app.use(express.json());
 // - reads data
 app.get("/api/people", (req, res) => {
-  res.status(200).json({success: true, data: people})
-})
+  res.status(200).json({ success: true, data: people });
+});
 // just because URLs the same doesn't mean the route is as the methods are diff
 // - adds data
 app.post("/api/people", (req, res) => {
-  const { name } = req.body
-  if(!name) {
-    return res.status(400).json("please provide a valid name")
+  const { name } = req.body;
+  if (!name) {
+    return res.status(400).json({ success: false, msg: "please enter a valid name" });
   } else {
-  res.status(201).send("SUCCESS") }
-})
+    res.status(201).json({success: true, msg: "SUCCESS" });
+  }
+});
 
 app.post("/login", (req, res) => {
-  const { name } = req.body
-  if(name) {
-    return res.status(200).send(`Welcome ${name}`)
+  const { name } = req.body;
+  if (name) {
+    return res.status(200).send(`Welcome ${name}`);
   }
   res.status(401).send("Please provide user credentials");
-})
+});
 
 app.listen(5005, () => {
   console.log("listening on port 5005");
