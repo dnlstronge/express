6f1e5c898f0c30e5546fb15c7cbf9a3b839a1de2
Dan Stronge <dnlstronge@gmail.com>
2023-03-14 11:08:24 +0000

    p[x] - show alternative to creating individual routes (chain the methods)

--- Routes/people.js
+++ Routes/people.js
@@ -8,11 +8,18 @@ const {
   deletePerson,
 } = require("../Controllers/people");
 
+/*
 router.get("/", getPeople);
-
 router.post("/", createPerson);
 router.post("/postman", createPersonPostman);
 router.put("/:id", updatePerson);
 router.delete("/:id", deletePerson);
+* */
+
+/* you can set up routes another way with the same funcitonality => */
+
+router.route("/").get(getPeople).post(createPerson)
+router.route("/postman").post(createPersonPostman)
+router.route("/:id").put(updatePerson).delete(deletePerson)
 
 module.exports = router;
