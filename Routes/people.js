fa682f89abe912b0f6eb012b1aa70014dc6f540e
Dan Stronge <dnlstronge@gmail.com>
2023-03-14 08:36:14 +0000

    [x] - move routes into separate files, [x] - implement express router

---
+++ Routes/people.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const router = express.Router();
+
+
+router.get("/api/people", (req, res) => {
+    res.status(200).json({ success: true, data: people });
+  });
+  
+  router.post("/api/people", (req, res) => {
+    const { name } = req.body;
+    if (!name) {
+      return res
+        .status(400)
+        .json({ success: false, msg: "please enter a valid name" });
+    } else {
+      res.status(201).json({ success: true, person: name, msg: "SUCCESS" });
+    }
+  });
+ router.post("/api/people/postman", (req, res) => {
+    const { name } = req.body;
+    if (name) {
+      return res
+        .status(201)
+        .json({ success: true, data: [...people, { id: 4, name: name }] });
+    } else {
+      return res
+        .status(401)
+        .json({ succes: false, msg: "Error: please enter a name" });
+    }
+  });
+  
+  
+  
+ router.put("/api/people/:id", (req, res) => {
+    const { id } = req.params; // id specified from browser
+    const { name } = req.body; // body - what you want to change to
+    const person = people.find((person) => person.id === Number(id));
+    if (!person) {
+      res
+      .status(404)
+      .json({success: false, msg: `id: ${id} does not exist`});
+    }
+    const newPeople = people.map((person) => {
+      if(person.id === Number(id)) {
+        person.name = name
+      }
+      return person;
+    })
+    res.status(200).json({success: true, data: newPeople})
+  })
+  
+  // when deleting not expecting anything in the body
+  app.delete("/api/people/:id", (req, res) => {
+    const person = people.find((person) => person.id === Number(req.params.id))
+    if(!person) {
+      res
+      .status(404)
+      .json({success: false, msg: `id: ${req.params.id} does not exist`})
+    }
+    const newPeople = people.filter(person => person.id !== Number(req.params.id))
+    return res.status(200).json({success: true, data: newPeople})
+  }) 
+  
--- app.js
+++ app.js
@@ -9,34 +9,8 @@ app.use(express.static("./methods-public"));
 app.use(express.urlencoded({ extended: false }));
 // parse json (more middleware)
 app.use(express.json());
-// - reads data
-app.get("/api/people", (req, res) => {
-  res.status(200).json({ success: true, data: people });
-});
-// just because URLs the same doesn't mean the route is as the methods are diff
-// - adds data
-app.post("/api/people", (req, res) => {
-  const { name } = req.body;
-  if (!name) {
-    return res
-      .status(400)
-      .json({ success: false, msg: "please enter a valid name" });
-  } else {
-    res.status(201).json({ success: true, person: name, msg: "SUCCESS" });
-  }
-});
-app.post("/api/people/postman", (req, res) => {
-  const { name } = req.body;
-  if (name) {
-    return res
-      .status(201)
-      .json({ success: true, data: [...people, { id: 4, name: name }] });
-  } else {
-    return res
-      .status(401)
-      .json({ succes: false, msg: "Error: please enter a name" });
-  }
-});
+
+
 
 app.post("/login", (req, res) => {
   const { name } = req.body;
@@ -45,36 +19,6 @@ app.post("/login", (req, res) => {
   }
   res.status(401).send("Please provide user credentials");
 });
-
-app.put("/api/people/:id", (req, res) => {
-  const { id } = req.params; // id specified from browser
-  const { name } = req.body; // body - what you want to change to
-  const person = people.find((person) => person.id === Number(id));
-  if (!person) {
-    res
-    .status(404)
-    .json({success: false, msg: `id: ${id} does not exist`});
-  }
-  const newPeople = people.map((person) => {
-    if(person.id === Number(id)) {
-      person.name = name
-    }
-    return person;
-  })
-  res.status(200).json({success: true, data: newPeople})
-})
-
-// when deleting not expecting anything in the body
-app.delete("/api/people/:id", (req, res) => {
-  const person = people.find((person) => person.id === Number(req.params.id))
-  if(!person) {
-    res
-    .status(404)
-    .json({success: false, msg: `id: ${req.params.id} does not exist`})
-  }
-  const newPeople = people.filter(person => person.id !== Number(req.params.id))
-  return res.status(200).json({success: true, data: newPeople})
-}) 
 
 // test in postman => delete functions 
 app.listen(5005, () => {
